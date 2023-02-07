dc800dcee3abb866167634120ae4c00ab1656b1a
abdellah targaoui <abdellah.targaoui@gmail.com>
2023-02-07 11:02:50 +0100

    finished post component designing

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react"
+import Post from "../post/Post"
 import Share from "../share/Share"
 import "./feed.css"
 
@@ -7,6 +8,10 @@ function Feed() {
     <div className="feed">
       <div className="feedWrapper">
         <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   )
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,17 +1,34 @@
 import React from "react"
 import "./post.css"
+import { MoreVert } from "@material-ui/icons"
 function Post() {
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img className="postProfileImg" src="/assest/person/1.jpeg" alt="" />
+            <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
+            <span className="postUsername">Abdellah targaoui</span>
+            <span className="postDate">5 min ago</span>
           </div>
-          <div className="postTopRight"></div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
-        <div className="postCenter"></div>
-        <div className="postBottom"></div>
+        <div className="postCenter">
+          <span className="postText">Hey there is my first post</span>
+          <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="BottomLeft">
+            <img className="likeIcon" src="/assets/like.png" alt="" />
+            <img className="likeIcon" src="/assets/heart.png" alt="" />
+            <span className="postLikeCounter">32 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments </span>
+          </div>
+        </div>
       </div>
     </div>
   )
